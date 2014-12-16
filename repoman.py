12e07989ed544f5faf46f27a8d5e9c8007a4298a
Edward Popoola <edwardpopoola@gmail.com>
2014-12-16 11:27:55 +0100

    Initial draft...sketch

---
+++ repoman.py
@@ -0,0 +1,45 @@
+import configparser
+
+
+class RepoMan():
+
+
+    def __init__():
+
+        self.action = null
+        self.uoptions = null
+        self.repofile = null
+
+
+    def parseRepoFile():
+        #return an enumerated biglist of available repository names
+        
+        repofile = "/etc/yum.repos.d/redhat.repo"
+        #check if file exists
+        config = configparser.ConfigParser()
+        config.read(repofile)
+        pass
+
+    def parseUserInput(inputType):
+        #clean the user input based on type
+        return uoptions and action
+        
+        pass
+
+    def processRequest(action, biglist, uoptions)
+        pass
+
+
+
+
+
+        
+def __main__():
+    pass
+
+
+
+
+
+
+
